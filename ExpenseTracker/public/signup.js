2070a92f0263c51cd51cea06e6d3dfe795ef62ff
Nishant Raj <79581018+raj-nishant@users.noreply.github.com>
2024-01-31 14:33:10 +0530

    commit

--- ExpenseTracker/public/signup.js
+++ ExpenseTracker/public/signup.js
@@ -3,49 +3,45 @@ const email = document.getElementById("email");
 const password = document.getElementById("password");
 const submitBtn = document.getElementById("submit");
 
-const BACKEND_ADDRESS = '54.147.138.102:3000';
+const BACKEND_ADDRESS = "localhost:3000";
 
 //=====================================================================================================
 
-
-submitBtn.addEventListener('click', postSignup);
-
+submitBtn.addEventListener("click", postSignup);
 
 //===================================================================================================
 
-
-
-async function postSignup(event){
-    event.preventDefault();
-    try
-    {
-        if(username.value=='' || email.value==''|| password.value==''){
-            return alert('please fill all the fields');
-        }
-
-        const signupObj = {
-            username: username.value,
-            email: email.value,
-            password: password.value
-        };
-        const response = await axios.post(`http://${BACKEND_ADDRESS}/user/signup`, signupObj);
-        if(response.status===201){
-            alert('user created successfully.\nKindly login now..');
-            clearUserForm();
-            window.location.href = "login.html";
-        }
-    }catch(err){
-        if (err.response) {
-            document.body.innerHTML += `<div style="color:red">${err}: ${err.response.data.error}</div>`;
-            return alert(err.response.data.error);
-        }
+async function postSignup(event) {
+  event.preventDefault();
+  try {
+    if (username.value == "" || email.value == "" || password.value == "") {
+      return alert("please fill all the fields");
     }
+
+    const signupObj = {
+      username: username.value,
+      email: email.value,
+      password: password.value,
+    };
+    const response = await axios.post(
+      `http://${BACKEND_ADDRESS}/user/signup`,
+      signupObj
+    );
+    if (response.status === 201) {
+      alert("user created successfully.\nKindly login now..");
+      clearUserForm();
+      window.location.href = "login.html";
+    }
+  } catch (err) {
+    if (err.response) {
+      document.body.innerHTML += `<div style="color:red">${err}: ${err.response.data.error}</div>`;
+      return alert(err.response.data.error);
+    }
+  }
 }
 
-
-
-function clearUserForm(){
-    username.value='';
-    email.value='';
-    password.value='';
+function clearUserForm() {
+  username.value = "";
+  email.value = "";
+  password.value = "";
 }
